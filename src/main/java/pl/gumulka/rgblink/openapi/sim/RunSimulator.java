1f0419cff908f39a16f36dff5bede029d3a552ac
Marcin Gumułka <marcingum@gmail.com>
2025-08-14 08:13:36 +0200

    small changes for mini-edge SDI

--- src/main/java/pl/gumulka/rgblink/openapi/sim/RunSimulator.java
+++ src/main/java/pl/gumulka/rgblink/openapi/sim/RunSimulator.java
@@ -6,11 +6,23 @@ import java.net.DatagramSocket;
 import java.net.InetAddress;
 import java.net.SocketException;
 import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
 
 public class RunSimulator {
   private DatagramSocket socket;
   private boolean running;
   private byte[] buf = new byte[256];
+
+  private static final List<String> KNOWN_DEVICE_MODEL_KEYS = Arrays.asList(
+      "220000", "320000", "700000", "A00000", "800000", "230100",
+      "240100", "240000", "241000", "250000", "629000", "2C0100",
+      "2C0106", "2C0107", "271000", "271001", "271100", "271101",
+      "353200"
+  );
+
+  private static final Random RANDOM = new Random();
 
   public static void main(String args[]) {
     try {
@@ -57,7 +69,9 @@ public class RunSimulator {
     String toSend = received.replaceFirst("T", "F");
 
 
-    toSend = addTao1ProDatablocks((toSend));
+    //toSend = addTao1ProDatablocks((toSend));
+    toSend = randomDeviceModelKey(toSend);
+    toSend = doSpecial7502stupidResponse(toSend);
 
     // fix checksum
     int sum = 0;
@@ -75,12 +89,35 @@ public class RunSimulator {
     return packet;
   }
 
+  private String doSpecial7502stupidResponse(String toSend) {
+    if (toSend.indexOf("7502") == 6) {
+      toSend = toSend.substring(0, 10) + "000000" + toSend.substring(16);
+    }
+    return toSend;
+  }
+
   private String addTao1ProDatablocks(String msg) {
     if (msg.matches("<F....F1B3.*")) {
       msg = msg.substring(0, 12) + "0700" + "00>" + "01 80 07 38 04 3c 00";
     } else if (msg.matches("<F....F1B4.*")) {
       msg = msg.substring(0, 12) + "2100" + "00>" + "00 22 72 74 6d 70 3a 2f 2f 31 39 32 2e 31 36 38 2e 30 2e 37 36 2f 6c 69 76 65 2f 74 65 73 74 22 cf";
+    } else if (msg.matches("<F....F1B5.*")) {
+      msg = msg.substring(0, 12) + "0500" + "00>" + "00 18 70 17 9f";
+    } else if (msg.matches("<F....F145.*")) {
+      msg = msg.substring(0, 12) + "2600" + "00>" + "2F 6D 65 64 69 61 2F 75 73 62 30 2F 72 65 63 6F 72 64 5F 32 30 31 33 30 31 31 38 30 39 35 37 34 35 2E 6D 70 34 F2";
     }
     return msg;
+  }
+
+  private String randomDeviceModelKey(String toSend) {
+    String target = "6801";
+    int index = toSend.indexOf(target);
+    if (index != -1 && index + 4 + 6 <= toSend.length()) {
+      int startReplace = index + 4;
+      int endReplace = startReplace + 6;
+      String randomKey = KNOWN_DEVICE_MODEL_KEYS.get(RANDOM.nextInt(KNOWN_DEVICE_MODEL_KEYS.size()));
+      return toSend.substring(0, startReplace) + randomKey + toSend.substring(endReplace);
+    }
+    return toSend;
   }
 }
